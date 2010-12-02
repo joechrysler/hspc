d54e7bb9cb1d99c93c3e421c7dda5891a012635b
Dylan Swartz <djswartz@svsu.edu>
2010-12-02 14:30:12 -0500

    Aaron's datamaper update. I started functions...but didn't do much.

--- main.rb
+++ main.rb
@@ -8,84 +8,127 @@ require 'dm-migrations'
 # A MySQL connection:
 DataMapper.setup(:default, 'mysql://hspc:bidgoli123@localhost/hspc')
 
+class Problem
+  include DataMapper::Resource
+  property :id,                   Serial
+  property :description,          Text
+  property :input,                Text
+  property :output,               Text
+  
+  has n, :Solution
+
+  def register()
+
+  end
+
+  def lookup()
+
+  end
+
+end
+
 class Competition
   include DataMapper::Resource
 
-  property :competition_id,       Integer,  :key => true
+  property :id,       Integer,  :key => true
   property :name,                 String,   :length => 35
   property :date,                 DateTime
   property :type,                 Enum[:test, :school]
   property :duration,             Integer
   property :location,             String
+end
+
+class ProblemSet
+  include DataMapper::Resource
+  property :id, Integer, :key =>true
   
-  #has 1, :problem_sets,           Integer
+  belongs_to :Problem ,      :key => true
+  has n, :Competition 
 end
 
 class FAQ
   include DataMapper::Resource
 
-  property :faq_id,               Integer, :key => true
+  property :id,                   Integer, :key => true
   property :question,             Text
   property :answer,               Text
   property :status,               Enum[:active, :inactive]
 end
 
-class Participant
+class School
+  include DataMapper::Resource
+  property :id,                   Integer, :key => true
+  property :name,                 String,   :length => 45
+  property :address,              String,   :length => 135
+  property :address2,              String,   :length => 135
+  property :status,               Enum[:active, :inactive]
+  property :phone,                String,   :length => 13
+  property :code,                 String,   :length => 8
+
+end
+
+class Zip
+  include DataMapper::Resource
+  property :code,                 Integer, :key => true
+  property :city,                 String
+  property :state,                String
+
+  has n, :School
+end
+
+class Coach
   include DataMapper::Resource
 
-  property :participant_id,       Integer,  :key => true
+  property :id,                   Integer,  :key => true
   property :name,                 String,   :length => 45
   property :address,              String,   :length => 45
   property :email,                String,   :length => 45
-  property :role,                 Enum[:coach, :contestant]
   property :shirt_size,           Enum[:XS, :S, :M, :L, :XL, :XXL]
   property :phone,                String,   :length => 13
   property :password,             String,   :length => 32
+end  
   
-  #belongs_to :team
-end
-
-class Problem
+class Participant
   include DataMapper::Resource
 
-  property :problem_id,           Integer, :key => true
-  property :description,          Text
-  property :input,                Text
-  property :output,               Text
-  
-  #belongs_to :problem_sets
-end
-
-class Problem_Set
-  include DataMapper::Resource
-
-  property :problem_id,           Integer, :key => true
-  property :problem_set,          Integer, :key => true
-end
-
-class School
-  include DataMapper::Resource
-  property :school_id,            Integer, :key => true
+  property :id,       Integer,  :key => true
   property :name,                 String,   :length => 45
-  property :address,              String,   :length => 135
-  property :status,               Enum[:active, :inactive]
-  property :phone,                String,   :length => 13
+  property :email,                String,   :length => 45
+  property :shirt_size,           Enum[:XS, :S, :M, :L, :XL, :XXL]
+  property :password,             String,   :length => 32
+
+  belongs_to :team
 end
 
 class Team
   include DataMapper::Resource
 
-  property :participant_id,      Integer,  :key => true
-  property :name,                String,   :length => 45
-  property :coach,               Integer
-  property :competition_id,      Integer
-  property :member1,             Integer
-  property :member2,             Integer
-  property :member3,             Integer
-  property :type,                Enum[:school, :test]
+  property :id,      Integer,  :key => true
+  property :name,    String,   :length => 45
+  property :type,    Enum[:school, :test]
+
+  belongs_to :Coach
+  belongs_to :School
+  belongs_to :Competition
 end
 
-#DataMapper.auto_upgrade!
+class Solution
+  include DataMapper::Resource
+  property :id, Serial
+  property :code, Text
+  
+end
+
+class Language
+  include DataMapper::Resource
+  property :id, Serial
+  property :name, String
+  
+  has n, :Solution
+end
+
+DataMapper.finalize
+#DataMapper.auto_migrate!
 
 get '/' do
   haml :index
